6048eb29ac9c1990ab374f607d3e5cde60d35c09
cc6715 <cc6715@ic.ac.uk>
2018-02-10 14:11:55 +0000

    bot can reply to submissions

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import praw
+from src import bot
+
 
 if __name__ == "__main__":
-    print("hello world")
-    reddit = praw.Reddit('bot1')
-    subreddit = reddit.subreddit("learnpython")
-
-    for submission in subreddit.hot(limit=5):
-        print("Title: ", submission.title)
-        print("Text: ", submission.selftext)
-        print("Score: ", submission.score)
-        print("---------------------------------\n")
+    bot.reply()
--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,29 @@
 import praw
+import pdb
+import re
+import os
 
-reddit = praw.Reddit('bot1')
-subreddit = praw.Reddit("learnPython")
+
+def reply():
+
+    reddit = praw.Reddit('bot1')
+    subreddit = reddit.subreddit("pythonforengineers")
+
+    if not os.path.isfile("posts_replied_to.txt"):
+        posts_replied_to = []
+    else:
+        with open("posts_replied_to.txt") as f:
+            posts_replied_to = f.read()
+            posts_replied_to = posts_replied_to.split("\n")
+            posts_replied_to = list(filter(None, posts_replied_to))
+
+    for submission in subreddit.hot(limit=5):
+        if submission.id not in posts_replied_to:
+            if re.search("testing bot replying", submission.title, re.IGNORECASE):
+                submission.reply("replying to this")
+                print("Bot replying to: " + submission.title)
+                posts_replied_to.append(submission.id)
+
+        with open("posts_replied_to.txt", "w") as f:
+            for post_id in posts_replied_to:
+                f.write(post_id + "\n")
