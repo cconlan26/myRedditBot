9a15b21255dd25bec6003a59c234c0a1c41091ae
cc6715 <cc6715@ic.ac.uk>
2018-02-10 14:16:48 +0000

    renaming post id file

--- src/bot.py
+++ src/bot.py
@@ -9,21 +9,21 @@ def reply():
     reddit = praw.Reddit('bot1')
     subreddit = reddit.subreddit("pythonforengineers")
 
-    if not os.path.isfile("posts_replied_to.txt"):
-        posts_replied_to = []
+    if not os.path.isfile("postIdHistory.txt"):
+        postIdHistory = []
     else:
-        with open("posts_replied_to.txt") as f:
-            posts_replied_to = f.read()
-            posts_replied_to = posts_replied_to.split("\n")
-            posts_replied_to = list(filter(None, posts_replied_to))
+        with open("postIdHistory.txt") as f:
+            postIdHistory = f.read()
+            postIdHistory = postIdHistory.split("\n")
+            postIdHistory = list(filter(None, postIdHistory))
 
     for submission in subreddit.hot(limit=5):
         if submission.id not in posts_replied_to:
             if re.search("testing bot replying", submission.title, re.IGNORECASE):
                 submission.reply("replying to this")
                 print("Bot replying to: " + submission.title)
-                posts_replied_to.append(submission.id)
+                postIdHistory.append(submission.id)
 
-        with open("posts_replied_to.txt", "w") as f:
-            for post_id in posts_replied_to:
+        with open("postIdHistory.txt", "w") as f:
+            for post_id in postIdHistory:
                 f.write(post_id + "\n")
